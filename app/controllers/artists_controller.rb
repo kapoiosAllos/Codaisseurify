d141db04c7c0690f3e7c23322c07a23175d70ddb
kapoiosAllos <kyriakos.z@yahoo.com>
2017-11-12 01:01:15 +0100

    added update artist and new artist form + delete button

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,4 +9,49 @@ class ArtistsController < ApplicationController
     @songs = @artist.songs
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist, notice: "Artist created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+     @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+
+    if @artist.update_attributes(artist_params)
+      redirect_to @artist, notice: "Artist updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+
+    @artist.destroy
+
+    redirect_to artists_path
+  end
+
+private
+
+  def artist_params
+    params
+          .require(:artist)
+          .permit(
+            :name
+          )
+  end
+
 end
